926a7978d010c952dc27eab516bed2a84cad7f77
kevinboco <kevinalexanderboca@gmail.com>
2025-07-18 01:12:11 -0500

    conexion para hostinger

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "proyecto_dinamico";
+$host = "mysql.hostinger.com";
+$user =  "u648222299_keboco4";    
+$pass = "Bucaramanga3011";
+$db = "u648222299_proyecto"; 
 
 $conexion = new mysqli($host, $user, $pass, $db);
 
